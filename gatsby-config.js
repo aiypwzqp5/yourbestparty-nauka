7e61669a066b40b9fa66ef161fa224add52d6fda
albert_strzyzewski_aa <albert.strzyzewski@sobre.pl>
2023-09-10 22:45:04 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `your-best-party`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
