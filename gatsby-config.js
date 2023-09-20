4bb6a18769cf023a8edc94176144f40be0c63789
albert_strzyzewski_aa <albert.strzyzewski@sobre.pl>
2023-09-20 22:26:48 +0200

    LEKCJA 10 - Konfiguracja DATOCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,17 @@
 module.exports = {
   siteMetadata: {
     title: `your-best-party`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        // You can find your read-only API token under the Settings > API tokens
+        // section of your administrative area. Make sure to grant both CDA and CMA permissions.
+        apiToken: process.env.GATSBY_DATO_CMS_KEY,
+      },
+    },
+  ],
 };
