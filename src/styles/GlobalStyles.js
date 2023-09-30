e96b384ddd49a719f864c884baa34813eaf570af
albert_strzyzewski_aa <albert.strzyzewski@sobre.pl>
2023-09-30 23:55:23 +0200

    LEKCJA 15: zmienne HTML

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  html {
+    --white: #fff;
+    --black: #000;
+    --orange: #EDAA52;
+    --red: #E94F37;
+    --light-gray: #F6F7EB;
+    --gray: #D9D9D9;
+    --dark-gray: #363535;
+    --green: #34DA31;
+    --cream: #F8ECEC; 
+  }
+
   * {
     margin: 0;
     padding: 0;
