85496b34fb9ddec06cc59570ddbbe2624da9b910
albert_strzyzewski_aa <albert.strzyzewski@sobre.pl>
2023-10-04 22:35:44 +0200

    LEKCJA 25: stworzenie Layout component

---
+++ src/components/templates/GlobalLayout/GlobalLayout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyles from "../../../styles/GlobalStyles";
+
+const GlobalLayout = ({ children }) => {
+  return (
+    <>
+      <GlobalStyles />
+      {children}
+    </>
+  );
+};
+
+export default GlobalLayout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,24 @@
 import * as React from "react";
-import GlobalStyle from "../styles/GlobalStyles";
 import Input from "../components/atoms/Input/Input";
 import Button from "../components/atoms/Button/Button";
 import HamburgerButton from "../components/atoms/HamburgerButton/HamburgerButton";
+import GlobalLayout from "../components/templates/GlobalLayout/GlobalLayout";
 
 const IndexPage = () => {
   return (
-    <main>
-      <GlobalStyle />
-      <Input name="example" label="Example" type="text" error="error text" />
-      <Input
-        name="example"
-        label="Example"
-        type="textarea"
-        error="error text"
-      />
-      <Button text="KONTAKT" />
-      <HamburgerButton />
-    </main>
+    <GlobalLayout>
+      <main>
+        <Input name="example" label="Example" type="text" error="error text" />
+        <Input
+          name="example"
+          label="Example"
+          type="textarea"
+          error="error text"
+        />
+        <Button text="KONTAKT" />
+        <HamburgerButton />
+      </main>
+    </GlobalLayout>
   );
 };
 
