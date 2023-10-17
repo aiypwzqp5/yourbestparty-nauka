0d9f72fd4a88f4fdebb0a29509110cec1e281574
albert_strzyzewski_aa <albert.strzyzewski@sobre.pl>
2023-10-18 01:05:28 +0200

    LEKCJA 36: wersja mobilna dla przycisku

--- src/components/atoms/Button/Button.styles.js
+++ src/components/atoms/Button/Button.styles.js
@@ -4,17 +4,23 @@ import { focusVisibleStyles } from "../../../styles/sharedStyles";
 export const StyledButton = styled.button`
   background-color: var(--orange);
   color: var(--white);
-  padding: 22px 62px;
+  padding: 12px 0;
   font-size: 20px;
   font-weight: 500;
   border-radius: 39px;
   border: none;
   cursor: pointer;
   transition: color 250ms linear, background-color 250ms linear;
+  width: 100%;
 
   &:hover {
     background-color: var(--dark-gray);
     color: var(--white);
+  }
+
+  @media only screen and (min-width: 768px) {
+    padding: 22px 62px;
+    max-width: 216px;
   }
 
   ${focusVisibleStyles};
